0134f1ddbb34b590d3ad890366635624341670da
Brian Young <berkshirehunt@gmail.com>
2021-07-06 15:10:24 -0700

    material ui style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './Nav'
-import { Card, Image, Heading } from 'rebass'
+import { Button, Menu, MenuItem, Fade} from '@material-ui/core'
+import {useState} from 'react'
+import { findAllInRenderedTree } from 'react-dom/cjs/react-dom-test-utils.production.min';
 
 function App() {
+  const  [menuOpen, setMenuOpen] = useState(null)
+
+  const handleClick = (e) => {
+    setMenuOpen(e.currentTarget)
+  }
+
   return (
     <div className="App">
-    <Nav />
-    {/* rebass styling */}
       <header className="App-header">
-        <Card width={120}>
-          <Image src={logo} />
-          <Heading>Learn React</Heading> 
-        </Card>
+        <Button style={{color: "white"}} aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+        </Button>
+        <Menu keepMounted open={menuOpen} TransitionComponent={Fade} anchorEl={menuOpen}>
+          <MenuItem onClick={handleClick}><a href="http://www.yahoo.com/">Option 1</a></MenuItem>
+          <MenuItem onClick={handleClick}>Option 2</MenuItem>
+          <MenuItem onClick={handleClick}>Option 3</MenuItem>
+        </Menu>
       </header>
     </div>
   )
