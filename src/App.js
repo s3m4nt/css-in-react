cc7397a87f676bead0a05e73259b4e123f8357d5
Brian Young <berkshirehunt@gmail.com>
2021-07-06 14:30:05 -0700

    rebass library example

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav'
+import { Card, Image, Heading } from 'rebass'
 
 function App() {
   return (
     <div className="App">
     <Nav />
+    {/* rebass styling */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card width={120}>
+          <Image src={logo} />
+          <Heading>Learn React</Heading> 
+        </Card>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
